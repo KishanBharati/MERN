4bfbebfad3cc0ff8cb90b7824e7346418eb7b2b6
KISHAN BHARATI <kishan.bharati.232106@gmail.com>
2025-02-03 17:28:33 +0530

    Introdusction to JavaScript

---
+++ Section1/javascript/condition.js
@@ -0,0 +1,19 @@
+num = 46;
+
+if(num % 2 ==0){
+    message = 'hello';
+    let message2 = 'Bye';
+    const pi = 3.14;
+    
+    // pi = 34.32;
+
+    console.log('even');
+
+
+} else{
+    console.log('odd');
+}
+
+console.log(message);
+// console.log(message2);
+
---
+++ Section1/javascript/index.js
@@ -0,0 +1,24 @@
+console.log('Hello World');
+
+a = 256;
+
+console.log(typeof a);
+
+console.log(a);
+
+mynmae = 'Kishan Bharati'
+
+console.log(typeof mynmae);
+
+console.log( 1+1 );
+console.log( '1' + '1' );
+console.log('1' + 1 + '1');
+console.log(true + '1');
+console.log(true + '1' + null + undefined);
+
+//exponential operator
+console.log(4 ** 3);
+
+console.log(1 == '1');
+console.log(1 === '1');
+
