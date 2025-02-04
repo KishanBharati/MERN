684e0389e1476fb4d8f7b0e682ab3b4ac90fb4fb
KISHAN BHARATI <kishan.bharati.232106@gmail.com>
2025-02-04 16:47:57 +0530

    Introduction to Loops in JS

---
+++ Section1/javascript/loops.js
@@ -0,0 +1,33 @@
+for(let i=0; i<10; i++){
+    console.log(i);    
+}
+
+//WAP a peogram to print all even numbers from 1 to 50
+
+for(i=1; i<=50; i++){
+    if(i%2 === 0){
+        console.log(i);        
+    }
+}
+
+//While Loop
+
+console.log('While Loop');
+
+let a=10;
+while(a<20){
+    console.log(a);
+    a++;
+}
+
+//Do While Loop
+
+console.log('Do While Loop');
+
+
+let b=10;
+do{
+    console.log(b);
+    b++;
+}
+while(b<10);
