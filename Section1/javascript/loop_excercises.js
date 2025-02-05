8f382c5c2eb099cc0fe452810460aff0dd6ad723
KISHAN BHARATI <kishan.bharati.232106@gmail.com>
2025-02-05 16:38:37 +0530

    loop excercise

---
+++ Section1/javascript/loop_excercises.js
@@ -0,0 +1,40 @@
+//WAP to print all numbers from 50 to 320 which are divisible by 7 and 11
+
+/*let a=50;
+for(i=50; i<=320; i++){
+    if(i%7 === 0 && i%11 === 0){
+        console.log(i);        
+    }
+}*/
+
+//WAP a program to Reverse a number 
+
+/*let n = 12345;
+let reverse = 0;
+while (n > 0) {
+    let d = n % 10;
+    reverse = reverse * 10 + d;
+    n = parseInt(n / 10);
+    console.log(reverse , n);    
+}
+console.log(reverse);*/
+
+
+// all Prime Number Range 40 to 130
+
+/*for (let i = 40; i <= 130; i++) {
+    let f = 0;
+    for (let j = 1; j <= i; j++) {
+        if (i % j === 0) {
+            f += 1;
+        } 
+        
+    }
+    if (f === 2){
+        console.log(i);            
+    }
+}*/
+
+
+
+
