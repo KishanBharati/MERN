d4ad3116e8e10cc75523e9a28afe14c469750e7d
KISHAN BHARATI <kishan.bharati.232106@gmail.com>
2025-02-13 18:27:28 +0530

    Creating and Using  Components

--- Section2/frontend/src/app/login/page.jsx
+++ Section2/frontend/src/app/login/page.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import classes from './login.module.css';
 
 const Login = () => {
   return (
     <div>
         <h1 className='text-center text-3xl'>Login page</h1>
+        <button className='btn'>login Button</button>
+        <button className={classes.loginBtn}>Submit Button</button>
     </div>
   )
 }
 
 export default Login;
+
+//rafce (for boiler plate)
--- Section2/frontend/src/app/page.jsx
+++ Section2/frontend/src/app/page.jsx
@@ -1,9 +1,25 @@
+import Button from '@/components/Button';
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
       <h1 className='text-center font-bold text-5xl'>Home page</h1>
+
+      <p style={{ color: 'red', fontSize: 30, fontWeight: 'bold' }} > 
+        Using Inline CSS 
+        </p>
+
+      <br />
+      <hr />
+      <input type="text" />
+      <button className='btn'>Submit</button>
+
+      <img src="/next.svg" alt="" />
+
+      <Button>Login</Button>
+      <Button disabled={true} >Signup</Button>
+
     </div>
   )
 }
