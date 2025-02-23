1ef15091f3db7e0625e7152b93bb6b1b8a73269e
KISHAN BHARATI <kishan.bharati.232106@gmail.com>
2025-02-23 18:27:59 +0530

    todoList

--- Section2/frontend/src/app/todolist/page.jsx
+++ Section2/frontend/src/app/todolist/page.jsx
@@ -1,4 +1,5 @@
 'use client'
+import { IconPencil, IconTrash } from '@tabler/icons-react';
 import React, { useState } from 'react'
 
 const TodoList = () => {
@@ -7,22 +8,34 @@ const TodoList = () => {
 
     // const [count, setCount] = useState(1)
 
-    const [taskList, setTaskList] = useState([
-         { text : 'Learn React', completed : false},
-         { text : 'Learn Express', completed : false},
-         { text : 'Learn MongoDB', completed : false},
-    ]);
+    const [taskList, setTaskList] = useState([]);
 
     const addNewTask = (e) => {
         if (e.code === 'Enter') {
             console.log(e.target.value);
 
+            const newTask = { text: e.target.value, completed: false };
 
-
+            setTaskList( [ newTask, ...taskList ]);
+            
             //clear input field
             e.target.valaue = '';
         }
     }
+
+        const deleteTask = (index) => {
+            console.log(index);  
+            
+            const temp = taskList;
+            temp.splice(index, 1);
+            setTaskList([...temp]);
+        }
+
+         const toggleTask = (index) => {
+            const temp = taskList;
+            temp[index].completed = !temp[index].completed;
+            setTaskList([...temp]);
+         }
 
     return (
         <div>
@@ -39,7 +52,7 @@ const TodoList = () => {
                             onKeyDown={addNewTask}
                         />
                     </div>
-                    <div className='p-5'>
+                    <div className='p-5 h-[60vh] overflow-auto'>
                         {/* <h1 className='text-2xl'>{count}</h1>
                             <button onClick={() => {setCount(count+1); console.log(count); }}>add count</button> */}
 
@@ -47,8 +60,29 @@ const TodoList = () => {
                                 taskList.map( (task, index) => {
                                     return (
                                         <div key={index} 
-                                            className='border md-3 rounded shadow p-4 text-white bg-blue-500'>
-                                            <p className='font-bold'>{task.text}</p>
+                                            className='flex justify-between border md-3 rounded shadow p-4 text-white bg-blue-500'>
+                                            <div>
+
+                                                {
+                                                    task.completed ? (
+                                                        <p className='w-fit px-2 text-sm bg-green-500 text-white rounded-full'>Done</p>
+                                                    ) : (
+                                                        <p className='w-fit px-2 text-sm bg-yellow-500 text-white rounded-full'>Not Done</p>
+                                                    )
+                                                }
+
+
+                                                <p className='font-bold'>{task.text}</p>
+                                            </div>                                           
+
+                                            <div className='flex gap-2'>
+                                                <button onClick={ () => { deleteTask(index) } } className='bg-white text-red-500 p-2 rounded'>
+                                                    <IconTrash/> 
+                                                </button>
+                                                <button onClick={ () => { toggleTask(index) }} className='bg-white text-blue-500 p-2 rounded'>
+                                                    <IconPencil/>
+                                                </button>
+                                            </div>
                                         </div>
                                     )
 
@@ -56,6 +90,10 @@ const TodoList = () => {
                             }
                     </div>
                 </div>
+
+                <p className='text-lg font-bold my-6 text-center'> 
+                    {taskList.filter(t => !t.completed).length} tasks pending 
+                </p>
 
             </div>
 
