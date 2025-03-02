9c4f56c4d8c5361b063dc5c59f754d6b412e90aa
KISHAN BHARATI <kishan.bharati.232106@gmail.com>
2025-03-02 18:27:03 +0530

    backend setup

--- Section3/backend/index.js
+++ Section3/backend/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const UserRouter = require('./routers/userRouter');
+
 const app = express();
 const port = 5000;
 
+//middleware
+app.use('/user', UserRouter);
+
+// endpoint or route
+app.get('/', (req, res) => {
+    res.send('response from express')
+});
+
+app.get('/add', (req, res) => {
+    res.send('response from add')
+});
+
+//add
+//getall
 
 app.listen(port, () => {
     console.log('server started'); 
