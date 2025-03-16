bc30168be0e028f50a391b549f32deb7e2786010
KISHAN BHARATI <kishan.bharati.232106@gmail.com>
2025-03-16 12:42:47 +0530

    data find, update, delete

--- Section3/backend/routers/userRouter.js
+++ Section3/backend/routers/userRouter.js
@@ -4,14 +4,13 @@ const Model = require('../models/userModel');
 const router = express.Router();
 
 router.post('/add', (req, res) =>{
-
-
-    console.log(req.body);
-    
-    new Model(req.body).save()
+    console.log(req.body);    
+    new Model(req.body)
+    .save()
     .then((result) => {
         res.status(200).json(result);
-    }).catch((err) => {
+    })
+    .catch((err) => {
         console.log(err);
         res.status(500).json(err);   
     });
@@ -33,19 +32,48 @@ router.get('/getbyemail', (req, res) => {
     res.send('response from user getbyemail');
 });
 
+// url parameter
+router.get("/getbycity/:city", (req, res) =>{
+    Model.find({ city: req.params.city })
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 //getbyid
-router.get('/getbyid', (req, res) => {
-    res.send('response from user getbyid');
+router.get('/getbyid/:id', (req, res) => {
+    Model.findById(req.paramas.id)
+        .then((result) => {
+            res.status(200).json (result);
+        }) .catch((err) =>{
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 //update
-router.get('/update', (req, res) => {
-    res.send('response from user update');
+router.put('/update/:id', (req, res) => {
+    Model.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);        
+    });
 });
 
 //delete
-router.get('/delete', (req, res) => {
-    res.send('response from user delete');
+router.delete('/delete/:id', (req, res) => {
+    Model.findByIdAndDelete(req.params.id)
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
